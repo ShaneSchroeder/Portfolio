d3299efc8b89d4c75d6236aaca77c8b72637c10d
ShaneSchroeder <76157271+ShaneSchroeder@users.noreply.github.com>
2023-01-20 22:30:53 -0500

    added some more cards, and fixed scrolling text to be a marquee instead of css

--- main.js
+++ main.js
@@ -1,21 +1,21 @@
 jQuery.easing['jswing'] = jQuery.easing['swing']; jQuery.extend(jQuery.easing, { easeOutBounce: function (x, t, b, c, d) { if ((t /= d) < (1 / 2.75)) { return c * (7.5625 * t * t) + b } else if (t < (2 / 2.75)) { return c * (7.5625 * (t -= (1.5 / 2.75)) * t + .75) + b } else if (t < (2.5 / 2.75)) { return c * (7.5625 * (t -= (2.25 / 2.75)) * t + .9375) + b } else { return c * (7.5625 * (t -= (2.625 / 2.75)) * t + .984375) + b } } });
 $(document).ready(function () {
     var previousMarginLeft = $("#aboutMePic").css("margin-left");
-    console.log(previousMarginLeft);
+    // console.log(previousMarginLeft);
     $("#aboutMe")
         .mouseenter(function () {
-            console.log(previousMarginLeft);
+            // console.log(previousMarginLeft);
             $(this).find("img").animate({
                 marginLeft: 0
             }, 500, "easeOutBounce");
-            $(".aboutMeScrollingText").remove();
+            $("#aboutMeScrollingText").remove();
         })
         .mouseleave(function () {
-           console.log("exiting...");
+           // console.log("exiting...");
            $(this).find("img").stop().animate({
             marginLeft: previousMarginLeft
         }, 500, "easeOutBounce");
-        $("#aboutMeScroll").prepend("<div class='scroll-text aboutMeScrollingText'>I'm Shane Schroeder, Software Engineer.</div>");
+        $("#aboutMeScroll").prepend("<marquee id='aboutMeScrollingText' behavior='scroll' direction='left' scrollamount='22'>I'm Shane Schroeder, Software Engineer.</marquee>");
         });
 
 });
